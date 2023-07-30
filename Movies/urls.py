af3dc324cb1c7aa0c4b422bdc8632dfbb8dfdc26
pujan tamang <pujantamang92@gmail.com>
2023-07-30 15:13:04 +0100

    changes in project structure

--- Movies/urls.py
+++ Movies/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+# import api config
+from .api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/', api.urls), # creating a url for Ninja API
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
